6f4f98076d2e744f486a629149d8824c97794976
Chxttr <49021789+Chxttr@users.noreply.github.com>
2020-08-23 14:21:23 +0200

    Added support for Discord Roles

--- client.lua
+++ client.lua
@@ -9,6 +9,85 @@ AddEventHandler('BadgerBankRobbery:IsActive:Return', function(bool)
 end)
 
 Citizen.CreateThread(function()
+	TriggerServerEvent('BadgerBankRobbery:CheckPerms')
+end)
+
+RegisterNetEvent('BadgerBankRobbery:bankblipalert')
+AddEventHandler('BadgerBankRobbery:bankblipalert', function()
+	local coords = GetEntityCoords(GetPlayerPed(-1))
+	for _, bankcoords in pairs(config.bankcoords) do
+		if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, bankcoords.x, bankcoords.y, bankcoords.z, true) < 5.0 then
+			TriggerServerEvent('PrintBR:PrintMessage', bankcoords.alarm)
+			if (config.displayBlips == true) then
+				bankcoords.blip = AddBlipForCoord(bankcoords.x, bankcoords.y, bankcoords.z)
+				SetBlipSprite(bankcoords.blip, 353)
+				SetBlipFlashTimer(bankcoords.blip, 1000 * config.timeToRob)
+				SetBlipAsShortRange(bankcoords.blip, true)
+				BeginTextCommandSetBlipName("STRING")
+				AddTextComponentString(bankcoords.name)
+				EndTextCommandSetBlipName(bankcoords.blip)
+			end
+		end
+	end
+	for _, ammunationcoords in pairs(config.ammunationcoords) do
+		if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, ammunationcoords.x, ammunationcoords.y, ammunationcoords.z, true) < 5.0 then
+			TriggerServerEvent('PrintBR:PrintMessage', ammunationcoords.alarm)
+			if (config.displayBlips == true) then
+				ammunationcoords.blip = AddBlipForCoord(ammunationcoords.x, ammunationcoords.y, ammunationcoords.z)
+				SetBlipSprite(ammunationcoords.blip, 353)
+				SetBlipFlashTimer(ammunationcoords.blip, 1000 * config.timeToRob)
+				SetBlipAsShortRange(ammunationcoords.blip, true)
+				BeginTextCommandSetBlipName("STRING")
+				AddTextComponentString(ammunationcoords.name)
+				EndTextCommandSetBlipName(ammunationcoords.blip)
+			end
+		end
+	end
+	for _, shopcoords in pairs(config.shopcoords) do
+		if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, shopcoords.x, shopcoords.y, shopcoords.z, true) < 5.0 then
+			TriggerServerEvent('PrintBR:PrintMessage', shopcoords.alarm)
+			if (config.displayBlips == true) then
+				shopcoords.blip = AddBlipForCoord(shopcoords.x, shopcoords.y, shopcoords.z)
+				SetBlipSprite(shopcoords.blip, 353)
+				SetBlipFlashTimer(shopcoords.blip, 1000 * config.timeToRob)
+				SetBlipAsShortRange(shopcoords.blip, true)
+				BeginTextCommandSetBlipName("STRING")
+				AddTextComponentString(shopcoords.name)
+				EndTextCommandSetBlipName(shopcoords.blip)
+			end
+		end
+	end
+	for _, ltdcoords in pairs(config.ltdcoords) do
+		if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, ltdcoords.x, ltdcoords.y, ltdcoords.z, true) < 5.0 then
+			TriggerServerEvent('PrintBR:PrintMessage', ltdcoords.alarm)
+			if (config.displayBlips == true) then
+				ltdcoords.blip = AddBlipForCoord(ltdcoords.x, ltdcoords.y, ltdcoords.z)
+				SetBlipSprite(ltdcoords.blip, 353)
+				SetBlipFlashTimer(ltdcoords.blip, 1000 * config.timeToRob)
+				SetBlipAsShortRange(ltdcoords.blip, true)
+				BeginTextCommandSetBlipName("STRING")
+				AddTextComponentString(ltdcoords.name)
+				EndTextCommandSetBlipName(ltdcoords.blip)
+			end
+		end
+	end
+	for _, liquorcoords in pairs(config.liquorcoords) do
+		if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, liquorcoords.x, liquorcoords.y, liquorcoords.z, true) < 5.0 then
+			TriggerServerEvent('PrintBR:PrintMessage', liquorcoords.alarm)
+			if (config.displayBlips == true) then
+				liquorcoords.blip = AddBlipForCoord(liquorcoords.x, liquorcoords.y, liquorcoords.z)
+				SetBlipSprite(liquorcoords.blip, 353)
+				SetBlipFlashTimer(liquorcoords.blip, 1000 * config.timeToRob)
+				SetBlipAsShortRange(liquorcoords.blip, true)
+				BeginTextCommandSetBlipName("STRING")
+				AddTextComponentString(liquorcoords.name)
+				EndTextCommandSetBlipName(liquorcoords.blip)
+			end
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 
@@ -51,7 +130,7 @@ Citizen.CreateThread(function()
 					DisplayNotification('~r~Press the ~w~E ~r~key to rob the bank')
 					if IsControlJustReleased(0, 38) then -- E key
 						TriggerServerEvent('BadgerBankRobbery:SetActive', true)
-						TriggerServerEvent('PrintBR:PrintMessage', bankcoords.alarm)
+						TriggerServerEvent('BadgerBankRobbery:bankrobberyalert')
 						if (config.displayBlips == true) then
 							bankcoords.blip = AddBlipForCoord(bankcoords.x, bankcoords.y, bankcoords.z)
 							SetBlipSprite(bankcoords.blip, 353)
@@ -103,8 +182,8 @@ Citizen.CreateThread(function()
 				if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, ammunationcoords.x, ammunationcoords.y, ammunationcoords.z) < 5.0 then
 					DisplayNotification('~r~Press the ~w~E ~r~key to rob the Ammunation')
 					if IsControlJustReleased(0, 38) then -- E
-						TriggerServerEvent('PrintBR:PrintMessage', ammunationcoords.alarm)
 						TriggerServerEvent('BadgerBankRobbery:SetActive', true)
+						TriggerServerEvent('BadgerBankRobbery:bankrobberyalert')
 						if (config.displayBlips == true) then
 							ammunationcoords.blip = AddBlipForCoord(ammunationcoords.x, ammunationcoords.y, ammunationcoords.z)
 							SetBlipSprite(ammunationcoords.blip, 353)
@@ -156,8 +235,8 @@ Citizen.CreateThread(function()
 				if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, shopcoords.x, shopcoords.y, shopcoords.z) < 5.0 then
 					DisplayNotification('~r~Press the ~w~E ~r~key to rob the 24/7')
 					if IsControlJustReleased(0, 38) then -- E
-						TriggerServerEvent('PrintBR:PrintMessage', shopcoords.alarm)
 						TriggerServerEvent('BadgerBankRobbery:SetActive', true)
+						TriggerServerEvent('BadgerBankRobbery:bankrobberyalert')
 						if (config.displayBlips == true) then
 							shopcoords.blip = AddBlipForCoord(shopcoords.x, shopcoords.y, shopcoords.z)
 							SetBlipSprite(shopcoords.blip, 353)
@@ -209,8 +288,8 @@ Citizen.CreateThread(function()
 				if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, ltdcoords.x, ltdcoords.y, ltdcoords.z) < 5.0 then
 					DisplayNotification('~r~Press the ~w~E ~r~key to rob the LTD Gas Station')
 					if IsControlJustReleased(0, 38) then -- E
-						TriggerServerEvent('PrintBR:PrintMessage', ltdcoords.alarm)
 						TriggerServerEvent('BadgerBankRobbery:SetActive', true)
+						TriggerServerEvent('BadgerBankRobbery:bankrobberyalert')
 						if (config.displayBlips == true) then
 							ltdcoords.blip = AddBlipForCoord(ltdcoords.x, ltdcoords.y, ltdcoords.z)
 							SetBlipSprite(ltdcoords.blip, 353)
@@ -262,8 +341,8 @@ Citizen.CreateThread(function()
 				if GetDistanceBetweenCoords(coords.x, coords.y, coords.z, liquorcoords.x, liquorcoords.y, liquorcoords.z) < 5.0 then
 					DisplayNotification('~r~Press the ~w~E ~r~key to rob the Liquor Store')
 					if IsControlJustReleased(0, 38) then -- E
-						TriggerServerEvent('PrintBR:PrintMessage', liquorcoords.alarm)
 						TriggerServerEvent('BadgerBankRobbery:SetActive', true)
+						TriggerServerEvent('BadgerBankRobbery:bankrobberyalert')
 						liquorcoords.blip = AddBlipForCoord(liquorcoords.x, liquorcoords.y, liquorcoords.z)
 						SetBlipSprite(liquorcoords.blip, 353)
 						SetBlipFlashTimer(liquorcoords.blip, 1000 * config.timeToRob)
